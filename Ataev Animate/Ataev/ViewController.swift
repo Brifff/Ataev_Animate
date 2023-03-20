be1a0e784990a86d40f6154a14d54135d0ae7a32
Ataev.m.N <104029906+Brifff@users.noreply.github.com>
2023-03-20 11:38:04 +0300

    animate init

---
+++ Ataev Animate/Ataev/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ataev
+//
+//  Created by Атаев Магарам Никматулаевич on 3/3/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
